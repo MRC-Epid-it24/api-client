ae4f20e0c0432041b7a876c57f1a9c2ba00165ef
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2018-01-15 14:50:57 +0000

    Added getFoodData

--- shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/client/roshttp/user/FoodDataImpl.scala
+++ shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/client/roshttp/user/FoodDataImpl.scala
@@ -6,7 +6,7 @@ import uk.ac.ncl.openlab.intake24.api.client.ApiError
 import uk.ac.ncl.openlab.intake24.api.client.roshttp.common.HttpServiceUtils
 import uk.ac.ncl.openlab.intake24.api.client.services.AuthRequestHandler
 import uk.ac.ncl.openlab.intake24.api.client.services.user.FoodDataService
-import uk.ac.ncl.openlab.intake24.api.data.LookupResult
+import uk.ac.ncl.openlab.intake24.api.data.{FoodDataForSurvey, LookupResult}
 
 import scala.concurrent.Future
 
@@ -17,4 +17,7 @@ class FoodDataImpl(val requestHandler: AuthRequestHandler) extends FoodDataServi
 
     requestHandler.sendWithAccessToken[LookupResult](request(Method.GET, s"/user/foods/$locale/lookup", queryParameters))
   }
+
+  def getFoodData(locale: String, code: String): Future[Either[ApiError, FoodDataForSurvey]] =
+    requestHandler.sendWithAccessToken[FoodDataForSurvey](request(Method.GET, s"/user/foods/$locale/$code"))
 }
--- shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/client/services/user/FoodDataService.scala
+++ shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/client/services/user/FoodDataService.scala
@@ -1,7 +1,7 @@
 package uk.ac.ncl.openlab.intake24.api.client.services.user
 
 import uk.ac.ncl.openlab.intake24.api.client.ApiError
-import uk.ac.ncl.openlab.intake24.api.data.LookupResult
+import uk.ac.ncl.openlab.intake24.api.data.{FoodDataForSurvey, LookupResult}
 
 import scala.concurrent.Future
 
@@ -9,4 +9,6 @@ trait FoodDataService {
 
   def lookup(locale: String, description: String, existing: Seq[String] = Seq(),
              limit: Option[Int] = None, algorithm: Option[String] = None): Future[Either[ApiError, LookupResult]]
+
+  def getFoodData(locale: String, code: String): Future[Either[ApiError, FoodDataForSurvey]]
 }
